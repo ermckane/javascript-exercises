f34eee17f9ed253b4d904f31b36ba7be8c0901ad
unknown <ermckane@gmail.com>
2022-09-13 20:47:05 -0400

    Working on js exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split('');
+    let reverseString = splitString.reverse();
+    let joinString = reverseString.join('');
+    return joinString;
 };
 
 // Do not edit below this line
